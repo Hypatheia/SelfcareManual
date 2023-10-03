b91c76ca8ecef40efa98cffafca60c198793d90b
feezessin <witt.nathaly@gmail.com>
2023-10-03 15:59:11 +0200

    edit file

--- script.js
+++ script.js
@@ -1,95 +1,68 @@
 const questionContainer = document.getElementById("question-container");
-const suggestionContainer = document.getElementById("suggestions-container");
-const continueButton = document.getElementById("continue-button");
+const suggestionContainer = document.getElementById("suggestions-container"); // Ensure the ID matches with the HTML element's ID
 
 let currentQuestionIndex = 0;
-let questions; // This variable will hold the fetched question data
-let currentQuestion;
+let questions; // Renamed the variable to 'questions' for clarity
 
 // Fetch the JSON data when the page loads
-fetch("/Users/nathaly/GitHub/Projects/SelfcareManual/questions.json")
-  .then((response) => response.json())
-  .then((data) => {
-    questions = data;
-    console.log(questions);
-    displayQuestion();
-  })
-  .catch((error) => {
-    console.error("Error fetching questions:", error);
-  });
+fetch("questions.json")  // Changed the path to a relative path
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        questions = data; // Corrected the variable name to 'questions'
+        displayQuestion();
+    })
+    .catch((error) => {
+        console.error("Error fetching questions:", error);
+    });
 
-// Function to display question, answers, responses, and suggestions
 function displayQuestion() {
-    if (currentQuestionIndex < questions.length) {
-        currentQuestion = questions[currentQuestionIndex]; // Assign the current question
-
-        const { question, options } = currentQuestion;
+    if (questions && questions.length > currentQuestionIndex) {
+        const currentQuestion = questions[currentQuestionIndex];
+        const { question, option } = currentQuestion; 
 
         questionContainer.textContent = question;
 
-        const answerButtons = options.map((option, index) => {
+        const answerButtons = option.map((option, index) => {  
             const button = document.createElement("button");
-            button.textContent = option.answers;
-            button.addEventListener("click", () => handleAnswerClick(index));
+            button.textContent = option.answer; 
+            button.addEventListener("click", () => handleAnswerClick(option, index));
             return button;
         });
 
         // Clear any previous answer buttons and add the new ones
-        suggestionContainer.textContent = ""; // Clear suggestion
-        continueButton.style.display = "none"; // Hide the continue button
-
         questionContainer.innerHTML = ""; // Clear previous content
         answerButtons.forEach((button) => questionContainer.appendChild(button));
 
-        console.log("Current question:", currentQuestion);
-    } else {
-        // Handle the case where there are no more questions
-        questionContainer.textContent = "Thank you for using the self-care manual!";
-        continueButton.style.display = "none"; // Hide the continue button
         suggestionContainer.textContent = ""; // Clear suggestion
+    } else {
+        suggestionContainer.textContent = "Thank you for using the self-care manual!";
     }
 }
 
-// Function to display a random suggestion
-function displayRandomSuggestion(suggestions) {
-    const randomIndex = Math.floor(Math.random() * suggestions.length);
-    suggestionContainer.textContent = suggestions[randomIndex];
-}
+function handleAnswerClick(option, index) { 
+    const response = option.response;
+    const suggestions = option.suggestion;
 
-// Function to handle user's answer click
-function handleAnswerClick(answerIndex) {
-    const currentQuestion = questions[currentQuestionIndex];
-    const option = currentQuestion.options[answerIndex];
-    const response = option.responses;
-    const suggestions = option.suggestions;
-
-    displayResponse(response);
+    suggestionContainer.textContent = response;
 
     // Check if the user's response should end the questionnaire
     const confirmResponse = confirm("Do you want to continue?");
 
     if (confirmResponse) {
-        displayRandomSuggestion(suggestions);
+        const randomIndex = Math.floor(Math.random() * suggestions.length); 
+        suggestionContainer.textContent += ' - ' + suggestions[randomIndex]; 
+
         currentQuestionIndex++;
-        displayQuestion(); // Display the next question
-        continueButton.style.display = "none"; // Hide the continue button
+        if (currentQuestionIndex < questions.length) {
+            displayQuestion();
+        } else {
+            suggestionContainer.textContent = "You've completed the questionnaire!"; 
+        }
     } else {
-        // User does not want to continue, you can handle this as needed
         suggestionContainer.textContent = "Thank you for using the self-care manual!";
-        continueButton.style.display = "none"; // Hide the continue button
     }
 }
 
-// Function to display a response
-function displayResponse(response) {
-    suggestionContainer.textContent = response;
-}
-
-// Function to handle the "Continue" button click
-continueButton.addEventListener("click", () => {
-    currentQuestionIndex++;
-    displayQuestion(); // Display the next question
-});
-  
-  // Start the questionnaire by displaying the first question
-  displayQuestion();
+// Start the questionnaire by displaying the first question
+displayQuestion();
