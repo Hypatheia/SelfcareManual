9c4e7052c79839bed65d82a0471f779b3591a5f5
feezessin <witt.nathaly@gmail.com>
2023-10-05 11:27:31 +0200

    edited code, added styles

--- script.js
+++ script.js
@@ -1,68 +1,96 @@
-const questionContainer = document.getElementById("question-container");
-const suggestionContainer = document.getElementById("suggestions-container"); // Ensure the ID matches with the HTML element's ID
-
+let quizData = [];  // Global variable to store the quiz data
 let currentQuestionIndex = 0;
-let questions; // Renamed the variable to 'questions' for clarity
 
-// Fetch the JSON data when the page loads
-fetch("questions.json")  // Changed the path to a relative path
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data);
-        questions = data; // Corrected the variable name to 'questions'
-        displayQuestion();
-    })
-    .catch((error) => {
-        console.error("Error fetching questions:", error);
-    });
-
-function displayQuestion() {
-    if (questions && questions.length > currentQuestionIndex) {
-        const currentQuestion = questions[currentQuestionIndex];
-        const { question, option } = currentQuestion; 
-
-        questionContainer.textContent = question;
-
-        const answerButtons = option.map((option, index) => {  
-            const button = document.createElement("button");
-            button.textContent = option.answer; 
-            button.addEventListener("click", () => handleAnswerClick(option, index));
-            return button;
-        });
-
-        // Clear any previous answer buttons and add the new ones
-        questionContainer.innerHTML = ""; // Clear previous content
-        answerButtons.forEach((button) => questionContainer.appendChild(button));
-
-        suggestionContainer.textContent = ""; // Clear suggestion
-    } else {
-        suggestionContainer.textContent = "Thank you for using the self-care manual!";
-    }
-}
-
-function handleAnswerClick(option, index) { 
-    const response = option.response;
-    const suggestions = option.suggestion;
-
-    suggestionContainer.textContent = response;
-
-    // Check if the user's response should end the questionnaire
-    const confirmResponse = confirm("Do you want to continue?");
-
-    if (confirmResponse) {
-        const randomIndex = Math.floor(Math.random() * suggestions.length); 
-        suggestionContainer.textContent += ' - ' + suggestions[randomIndex]; 
-
-        currentQuestionIndex++;
-        if (currentQuestionIndex < questions.length) {
-            displayQuestion();
-        } else {
-            suggestionContainer.textContent = "You've completed the questionnaire!"; 
+// Function to fetch the quiz data from questions.json
+async function fetchQuizData() {
+    try {
+        const response = await fetch('questions.json');
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
         }
-    } else {
-        suggestionContainer.textContent = "Thank you for using the self-care manual!";
+        quizData = await response.json();
+        showQuestion();  // Start the quiz once the data is loaded
+    } catch (error) {
+        console.error('Failed to fetch quiz data:', error);
     }
 }
 
-// Start the questionnaire by displaying the first question
-displayQuestion();
+function showQuestion() {
+    const questionData = quizData[currentQuestionIndex];
+    const quizContainer = document.getElementById('quiz-container');
+
+    // Clearing the previous content
+    quizContainer.innerHTML = '';
+
+    // Displaying the question
+    const questionElement = document.createElement('p');
+    questionElement.innerText = questionData.question;
+    quizContainer.appendChild(questionElement);
+
+    // Displaying the answers
+    questionData.option.forEach((option, index) => {
+        const answerButton = document.createElement('button');
+        answerButton.innerText = option.answer;
+        answerButton.classList.add('answer-btn');
+        answerButton.onclick = () => showResponse(index);
+        quizContainer.appendChild(answerButton);
+    });
+}
+
+function showResponse(answerIndex) {
+    const questionData = quizData[currentQuestionIndex];
+    const quizContainer = document.getElementById('quiz-container');
+
+    // Clearing the previous content
+    quizContainer.innerHTML = '';
+
+    // Displaying the response
+    const responseElement = document.createElement('p');
+    responseElement.innerText = questionData.option[answerIndex].response;
+    quizContainer.appendChild(responseElement);
+
+    // Displaying a random suggestion
+    const suggestionElement = document.createElement('p');
+    const randomSuggestion = questionData.option[answerIndex].suggestions[Math.floor(Math.random() * questionData.option[answerIndex].suggestions.length)];
+    suggestionElement.innerText = randomSuggestion;
+    quizContainer.appendChild(suggestionElement);
+
+    // Asking the user if they want to continue
+    const continueButton = document.createElement('button');
+    continueButton.innerText = 'Continue';
+    continueButton.classList.add('nav-btn');
+    continueButton.onclick = () => {
+        currentQuestionIndex++;
+        if (currentQuestionIndex < quizData.length) {
+            showQuestion();  // Show the next question
+        } else {
+            endQuiz();  // End the quiz if there are no more questions
+        }
+    };
+    quizContainer.appendChild(continueButton);
+
+    // Adding a button to exit the quiz
+    const exitButton = document.createElement('button');
+    exitButton.innerText = 'Exit';
+    exitButton.classList.add('nav-btn');
+    exitButton.onclick = endQuiz;
+    quizContainer.appendChild(exitButton);
+}
+
+function endQuiz() {
+    const quizContainer = document.getElementById('quiz-container');
+    quizContainer.innerHTML = '<button id="start-button" class="nav-btn" onclick="startQuiz()">Start Quiz</button>';
+}
+
+function startQuiz() {
+    if (quizData.length > 0) {
+        showQuestion();
+    } else {
+        fetchQuizData();  // Fetch the quiz data if it hasn't been loaded yet
+    }
+}
+
+// If you want to automatically start the quiz when the page loads,
+// uncomment the line below. Otherwise, the quiz will start when
+// the user clicks the "Start Quiz" button.
+// fetchQuizData();
